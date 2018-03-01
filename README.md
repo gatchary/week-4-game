efa61f0e6bb3b0ea14e24f2aa87612a78f5a3267
gatchary <gatchary@yahoo.com>
2018-03-01 04:27:30 -0400

    updated all files adding jquery to site, hard hard hard

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # week-4-game
+ current id tags mad 
+ #player1
+ #player2
+ #player3
+ #player4   
---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    
+
+});
+    
+    var player1 = $("#player1");
+    var player2 = $("#player2");
+    var player3 = $("#player3");
+    var player4 = $("#player4");
+    var enemies = $("#enemies");
+    var fighter = $("#fighter");
+    var opponent = $("#opponent");
+   
+    ().append(enemies)
+    ().append(fighter)
+    ().append(opponent)
+   
+    function playerSelect(){
+   
+       
+    }
+ 
+
