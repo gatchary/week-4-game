b18985ba2e528b6e7db99cfd02a8ff98d2e9b4f6
gatchary <gatchary@yahoo.com>
2018-03-03 04:21:51 -0400

    just alot more fixing the next step is to chose opponent than the math smh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,24 +9,52 @@ $(document).ready(function()
     var player4 = $("#player4");
     var players =[player1, player2, player3, player4];
     var opponent = [];
+    var fighter= [];
+    var enemies= [];
    // var that = this;
-
+ //  console.log(players);
+ //  console.log(fighter.length);
     //create function to move players / images from one div to fighter and
     $(".player").click( function ()
     {
-
+   //     console.log(players);
+// use the this keyword to select element clicked by assigning it to a variable 
     var playerSelected = $(this);
-    console.log(playerSelected);
+ //   console.log(playerSelected);
+    //append to the figher div 
  $("#fighter").append(playerSelected );
- console.log($("fighter"));
+ //console.log($("fighter"));
+ //remove the class from the selected element so i can have the others to use
  $(playerSelected ).removeClass( "player" );
- console.log(playerSelected);
- console.log($(".player"));
+ //push the selected fighter to an empty array for later use
+ fighter.push(playerSelected); 
+// console.log(fighter.length);
+ 
+console.log(playerSelected);
+// console.log("this is fighter array" + fighter)
+ 
+// console.log($(".player"));
+ // use for loop to append the remaining elements in the player array to enemies 
+ 
  var players = $(".player");
+ console.log("here it should have the class of player" + players);
+ 
  for (var i = 0; i < players.length; i++) {
     $("#enemies").append(players[i]);
+   // console.log(players[i]);
+   // console.log(players);
+    //push the remainder of the players array into the enemies array for later use
+    
 }
- 
+$(players).removeClass("player");
+enemies = players.slice(0);
+console.log(enemies.length);
+//$(enemies).removeClass("player");
+console.log(enemies);
+
+
+
+
 
  
 
@@ -34,7 +62,7 @@ $(document).ready(function()
 }
 
 ); 
-
+// create a click function to add enimies to opponent section of the game 
 
 }); 
 
