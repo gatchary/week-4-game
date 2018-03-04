758a585384e5d46c45222e2bd734ff119b467f07
gatchary <gatchary@yahoo.com>
2018-03-04 01:33:51 -0400

    now everything is in order of working, now for the worst part the math

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //documet ready
-$(document).ready(function()
+$({}).ready(function()
 {
     //creat variables
     var gameRunning = true;
@@ -15,29 +15,37 @@ $(document).ready(function()
  //  console.log(players);
  //  console.log(fighter.length);
     //create function to move players / images from one div to fighter and
-    $(".player").click( function ()
+    $(".player").on('click', function ()
     {
+      //  console.log($(".player"));
    //     console.log(players);
 // use the this keyword to select element clicked by assigning it to a variable 
-    var playerSelected = $(this);
- //   console.log(playerSelected);
+  var playerSelected = $(this);
+ // console.log(playerSelected);
+  $( ".player" ).off();
     //append to the figher div 
  $("#fighter").append(playerSelected );
- //console.log($("fighter"));
+// console.log($("#fighter"));
  //remove the class from the selected element so i can have the others to use
  $(playerSelected ).removeClass( "player" );
+
+ //console.log("this is after class was removed what is player here" + $( ".player"));
  //push the selected fighter to an empty array for later use
  fighter.push(playerSelected); 
-// console.log(fighter.length);
- 
-console.log(playerSelected);
+ //console.log("this is the fighter " + fighter);
+ //console.log("this is player selected " + playerSelected);
+ //console.log(fighter.length);
+
+ // a if statement for the fighter array to prevent adding more fighters 
+
+//console.log(playerSelected);
 // console.log("this is fighter array" + fighter)
  
 // console.log($(".player"));
  // use for loop to append the remaining elements in the player array to enemies 
  
  var players = $(".player");
- console.log("here it should have the class of player" + players);
+// console.log("here it should not have the class of player" + players);
  
  for (var i = 0; i < players.length; i++) {
     $("#enemies").append(players[i]);
@@ -46,28 +54,69 @@ console.log(playerSelected);
     //push the remainder of the players array into the enemies array for later use
     
 }
+// remove class of players from remaining 3 players 
 $(players).removeClass("player");
+// add the players to the enemy array 
 enemies = players.slice(0);
-console.log(enemies.length);
-//$(enemies).removeClass("player");
-console.log(enemies);
 
+console.log(enemies + enemies.length);
+//add new class to the players of enemies 
+$(enemies).addClass("enemies")
+console.log(players + players.length);
+if (fighter.length == 1) {
+    alert ("you have chosen youre warrior")
 
-
-
-
- 
-
- 
+} else {
+    
+    $("players").empty();
 }
+//$(enemies).removeClass("player");
 
-); 
-// create a click function to add enimies to opponent section of the game 
+
+
+
+
+
+ 
+$(".enemies").on('click', function (){
+    console.log("enemies");
+    console.log(".enemies");
+    
+   var enemySelected = $(this);
+   $("#opponent").append(enemySelected);
+     opponent.push(enemySelected); 
+       console.log(enemySelected);
+   console.log($("#opponent"));
+   console.log(fighter);
+    console.log(opponent);
+    console.log(enemies);
+   console.log(opponent.length);
+    console.log(enemies.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+ 
+}); 
+
+
 
 }); 
 
 
 
+// create a click function to add enimies to opponent section of the game 
 
 
 
